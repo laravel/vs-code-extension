a136d6e5376d204a6c4d309fb7bd3428493c25c0
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-06-04 14:24:18 -0400

    better inertia naming convention for deeper directories

--- src/repositories/inertia.ts
+++ src/repositories/inertia.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import * as vscode from "vscode";
 import { View } from "..";
-import { createFileWatcher } from "./../support/fileWatcher";
-import { projectPath } from "./../support/project";
+import { createFileWatcher } from "../support/fileWatcher";
+import { projectPath, relativePath } from "./../support/project";
 
 let views: {
     [key: string]: View;
@@ -10,13 +10,13 @@ let views: {
 
 const load = () => {
     ["/resources/js/Pages"].forEach((path) => {
-        collectViews(projectPath(path)).forEach((view) => {
+        collectViews(projectPath(path), path).forEach((view) => {
             views[view.name] = view;
         });
     });
 };
 
-const collectViews = (path: string): View[] => {
+const collectViews = (path: string, basePath: string): View[] => {
     if (path.substring(-1) === "/" || path.substring(-1) === "\\") {
         path = path.substring(0, path.length - 1);
     }
@@ -29,7 +29,7 @@ const collectViews = (path: string): View[] => {
         .readdirSync(path)
         .map((file: string) => {
             if (fs.lstatSync(`${path}/${file}`).isDirectory()) {
-                return collectViews(`${path}/${file}`);
+                return collectViews(`${path}/${file}`, basePath);
             }
 
             if (!file.match(/\.vue|\.tsx|\.jsx$/)) {
@@ -39,8 +39,11 @@ const collectViews = (path: string): View[] => {
             const name = file.replace(/\.(vue|tsx|jsx)$/, "");
 
             return {
-                name,
-                relativePath: `${path}/${name}`.replace(projectPath(""), ""),
+                name: relativePath(`${path}/${name}`).replace(
+                    basePath.substring(1) + "/",
+                    "",
+                ),
+                relativePath: relativePath(`${path}/${file}`),
                 uri: vscode.Uri.file(`${path}/${file}`),
             };
         })
