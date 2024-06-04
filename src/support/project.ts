ad02ab220242573f11b99b1e6cd67ec88ff3538c
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-06-04 11:16:43 -0400

    Update project.ts

--- src/support/project.ts
+++ src/support/project.ts
@@ -3,44 +3,38 @@ import * as path from "path";
 import * as vscode from "vscode";
 import { config } from "./config";
 
-export const projectPath = (relativePath = "", forCode = false): string => {
-    if (relativePath[0] === "/") {
-        relativePath = relativePath.substr(1);
+export const projectPath = (path = "", forCode = false): string => {
+    if (path[0] === "/") {
+        path = path.substring(1);
     }
 
     let basePath = config<string>("basePath", "");
 
     if (forCode === false && basePath.length > 0) {
-        if (basePath.startsWith(".") && hasWorkspace()) {
-            basePath = path.resolve(
-                getWorkspaceFolders()[0].uri.fsPath,
-                basePath,
-            );
-        }
-
-        return basePath.replace(/[\/\\]$/, "") + "/" + relativePath;
+        return resolvePath(basePath, path);
     }
 
     let basePathForCode = config<string>("basePathForCode", "");
 
     if (forCode && basePathForCode.length > 0) {
-        if (basePathForCode.startsWith(".") && hasWorkspace()) {
-            basePathForCode = path.resolve(
-                getWorkspaceFolders()[0].uri.fsPath,
-                basePathForCode,
-            );
-        }
-
-        return basePathForCode.replace(/[\/\\]$/, "") + "/" + relativePath;
+        return resolvePath(basePathForCode, path);
     }
 
     for (let workspaceFolder of getWorkspaceFolders()) {
         if (fs.existsSync(`${workspaceFolder.uri.fsPath}/artisan`)) {
-            return `${workspaceFolder.uri.fsPath}/${relativePath}`;
+            return `${workspaceFolder.uri.fsPath}/${path}`;
         }
     }
 
     return "";
+};
+
+const resolvePath = (basePath: string, relativePath: string): string => {
+    if (basePath.startsWith(".") && hasWorkspace()) {
+        basePath = path.resolve(getWorkspaceFolders()[0].uri.fsPath, basePath);
+    }
+
+    return `${basePath.replace(/[\/\\]$/, "")}/${relativePath}`;
 };
 
 export const hasWorkspace = (): boolean => {
