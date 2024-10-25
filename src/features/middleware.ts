b55c8b37589a5e6abe120512b6020511343e2335
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-10-25 11:08:56 -0400

    fix param'ed middleware diagnostics

--- src/features/middleware.ts
+++ src/features/middleware.ts
@@ -9,13 +9,14 @@ const diagnosticProvider = (
 ): Promise<vscode.Diagnostic[]> => {
     return findWarningsInDoc(doc, middlewareMatchRegex, (match, range) => {
         return getMiddleware().whenLoaded((items) => {
-            const item = items[match[0]];
+            const finalMatch = match[0].split(":").shift() ?? "";
+            const item = items[finalMatch];
 
             if (item) {
                 return null;
             }
 
-            return notFound("Middleware", match[0], range, "middleware");
+            return notFound("Middleware", finalMatch, range, "middleware");
         });
     });
 };
