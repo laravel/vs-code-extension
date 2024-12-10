a8be2efc4e610c9da584f09f05d2272420e469f4
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-12-10 12:55:33 -0500

    Update parser.ts

--- src/support/parser.ts
+++ src/support/parser.ts
@@ -47,7 +47,7 @@ export const setParserBinaryPath = (context: vscode.ExtensionContext) => {
 const downloadBinary = async (context: vscode.ExtensionContext) => {
     const binaryVersion = "0.1.1";
     const filename = `php-parser-${binaryVersion}`;
-    const uri = `https://github.com/joetannenbaum/php-parser-cli/raw/refs/heads/main/bin/${filename}`;
+    const uri = `https://github.com/laravel/vs-code-php-parser-cli/raw/refs/heads/main/bin/${filename}`;
 
     const fileDownloader: FileDownloader = await getApi();
 
