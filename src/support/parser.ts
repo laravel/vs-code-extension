c4976133cbfb909e8098fb9f5c57c52d406f861d
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-12-19 12:22:04 -0500

    Update parser.ts

--- src/support/parser.ts
+++ src/support/parser.ts
@@ -37,7 +37,7 @@ export const setParserBinaryPath = (context: vscode.ExtensionContext) => {
 };
 
 const downloadBinary = async (context: vscode.ExtensionContext) => {
-    const binaryVersion = "0.1.32";
+    const binaryVersion = "0.1.34";
     const osPlatform = os.platform();
     const osArch = os.arch();
     const extension = osPlatform === "win32" ? ".exe" : "";
