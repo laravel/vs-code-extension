9e65a904e0e1e5ebaae151e3af14866da9d1987a
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-01-06 10:09:41 -0500

    add debounce util

--- src/support/util.ts
+++ src/support/util.ts
@@ -58,3 +58,18 @@ export const cleanUpTemp = () => {
         fs.rmdirSync(tempDir, { recursive: true });
     }
 };
+
+export const debounce = <T extends (...args: any[]) => any>(
+    func: T,
+    wait: number,
+): T => {
+    let timeout: NodeJS.Timeout;
+
+    return function (this: any, ...args: any[]) {
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, wait);
+    } as T;
+};
