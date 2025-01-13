8a8f5e907dcd1abca8c0abca9f02ca6bb1c82f12
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-01-13 15:56:59 -0500

    account for json translation files

--- php-templates/translations.php
+++ php-templates/translations.php
@@ -1,93 +1,127 @@
 <?php
 
+function vsCodeGetJsonTranslationsFromFile($lang, $key, $value, $file, $lines)
+{
+    return [
+        "k" => $key,
+        "la" => $lang,
+        "vs" => collect(\Illuminate\Support\Arr::dot((\Illuminate\Support\Arr::wrap(__($key, [], $lang)))))
+            ->map(
+                fn($value, $k) => vsCodeTranslationValue(
+                    $key,
+                    $value,
+                    $file,
+                    $lines
+                )
+            )
+            ->filter()
+    ];
+}
+
 function vsCodeGetTranslationsFromFile($file, $path, $namespace)
 {
-  $key = pathinfo($file, PATHINFO_FILENAME);
+    $fileLines = \Illuminate\Support\Facades\File::lines($file);
+    $lines = [];
+    $inComment = false;
 
-  if ($namespace) {
-    $key = "{$namespace}::{$key}";
-  }
+    foreach ($fileLines as $index => $line) {
+        $trimmed = trim($line);
 
-  $lang = collect(explode(DIRECTORY_SEPARATOR, str_replace($path, "", $file)))
-    ->filter()
-    ->first();
+        if (substr($trimmed, 0, 2) === "/*") {
+            $inComment = true;
+            continue;
+        }
 
-  $fileLines = \Illuminate\Support\Facades\File::lines($file);
-  $lines = [];
-  $inComment = false;
+        if ($inComment) {
+            if (substr($trimmed, -2) !== "*/") {
+                continue;
+            }
 
-  foreach ($fileLines as $index => $line) {
-    $trimmed = trim($line);
+            $inComment = false;
+        }
 
-    if (substr($trimmed, 0, 2) === "/*") {
-      $inComment = true;
-      continue;
+        if (substr($trimmed, 0, 2) === "//") {
+            continue;
+        }
+
+        $lines[] = [$index + 1, $trimmed];
     }
 
-    if ($inComment) {
-      if (substr($trimmed, -2) !== "*/") {
-        continue;
-      }
+    if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+        $lang = pathinfo($file, PATHINFO_FILENAME);
 
-      $inComment = false;
+        return collect(\Illuminate\Support\Facades\File::json($file))->map(
+            fn($value, $key) => vsCodeGetJsonTranslationsFromFile(
+                $lang,
+                $key,
+                $value,
+                vsCodeToRelativePath($file),
+                $lines,
+            ),
+        );
     }
 
-    if (substr($trimmed, 0, 2) === "//") {
-      continue;
+    $key = pathinfo($file, PATHINFO_FILENAME);
+
+    if ($namespace) {
+        $key = "{$namespace}::{$key}";
     }
 
-    $lines[] = [$index + 1, $trimmed];
-  }
+    $lang = collect(explode(DIRECTORY_SEPARATOR, str_replace($path, "", $file)))
+        ->filter()
+        ->slice(-2, 1)
+        ->first();
 
-  return [
-    "k" => $key,
-    "la" => $lang,
-    "vs" => collect(\Illuminate\Support\Arr::dot((\Illuminate\Support\Arr::wrap(__($key, [], $lang)))))
-      ->map(
-        fn($value, $key) => vsCodeTranslationValue(
-          $key,
-          $value,
-          str_replace(base_path(DIRECTORY_SEPARATOR), "", $file),
-          $lines
-        )
-      )
-      ->filter()
-  ];
+    return [
+        "k" => $key,
+        "la" => $lang,
+        "vs" => collect(\Illuminate\Support\Arr::dot((\Illuminate\Support\Arr::wrap(__($key, [], $lang)))))
+            ->map(
+                fn($value, $key) => vsCodeTranslationValue(
+                    $key,
+                    $value,
+                    vsCodeToRelativePath($file),
+                    $lines
+                )
+            )
+            ->filter()
+    ];
 }
 
 function vsCodeTranslationValue($key, $value, $file, $lines): ?array
 {
-  if (is_array($value)) {
-    return null;
-  }
+    $isJson = pathinfo($file, PATHINFO_EXTENSION) === 'json';
 
-  $lineNumber = 1;
-  $keys = explode(".", $key);
-  $index = 0;
-  $currentKey = array_shift($keys);
-
-  foreach ($lines as $index => $line) {
-    if (
-      strpos($line[1], '"' . $currentKey . '"', 0) !== false ||
-      strpos($line[1], "'" . $currentKey . "'", 0) !== false
-    ) {
-      $lineNumber = $line[0];
-      $currentKey = array_shift($keys);
+    if (is_array($value)) {
+        return null;
     }
 
-    if ($currentKey === null) {
-      break;
-    }
-  }
+    $lineNumber = 1;
+    $keys = $isJson ? [$key] : explode(".", $key);
+    $currentKey = array_shift($keys);
 
-  return [
-    "v" => $value,
-    "p" => $file,
-    "li" => $lineNumber,
-    "pa" => preg_match_all("/\:([A-Za-z0-9_]+)/", $value, $matches)
-      ? $matches[1]
-      : []
-  ];
+    foreach ($lines as $line) {
+        if (
+            strpos($line[1], '"' . $currentKey . '"', 0) !== false ||
+            strpos($line[1], "'" . $currentKey . "'", 0) !== false
+        ) {
+            $lineNumber = $line[0];
+            $currentKey = array_shift($keys);
+        }
+
+        if ($currentKey === null) {
+            break;
+        }
+    }
+
+    return [
+        "v" => $value,
+        "p" => $file,
+        "li" => $lineNumber,
+        "pa" => preg_match_all("/\:([A-Za-z0-9_]+)/", $value, $matches)
+            ? $matches[1]
+            : []
+    ];
 }
 
 function vscodeCollectTranslations(string $path, ?string $namespace = null)
@@ -102,6 +136,7 @@ function vscodeCollectTranslations(string $path, ?string $namespace = null)
         fn($file) => vsCodeGetTranslationsFromFile($file, $path, $namespace)
     );
 }
+
 
 $loader = app("translator")->getLoader();
 $namespaces = $loader->namespaces();
@@ -123,29 +158,38 @@ if ($property !== null) {
 }
 
 $default = collect($paths)->flatMap(
-  fn($path) => vscodeCollectTranslations($path)
+    fn($path) => vscodeCollectTranslations($path)
 );
 
 $namespaced = collect($namespaces)->flatMap(
-  fn($path, $namespace) => vscodeCollectTranslations($path, $namespace)
+    fn($path, $namespace) => vscodeCollectTranslations($path, $namespace)
 );
 
 $final = [];
 
 foreach ($default->merge($namespaced) as $value) {
-  foreach ($value["vs"] as $key => $v) {
-    $dotKey = "{$value["k"]}.{$key}";
+    if ($value instanceof \Illuminate\Support\Collection) {
+        foreach ($value as $val) {
+            foreach ($val["vs"] as $key => $v) {
+                $dotKey = $val["k"];
 
-    if (!isset($final[$dotKey])) {
-      $final[$dotKey] = [];
+                if (!isset($final[$dotKey])) {
+                    $final[$dotKey] = [];
+                }
+
+                $final[$dotKey][$val["la"]] = $v;
+            }
+        }
+    } else {
+        foreach ($value["vs"] as $key => $v) {
+            $dotKey = "{$value["k"]}.{$key}";
+            $final[$dotKey] ??= [];
+            $final[$dotKey][$value["la"]] ??= $v;
+        }
     }
-
-    $final[$dotKey][$value["la"]] = $v;
-
-    if ($value["la"] === \Illuminate\Support\Facades\App::currentLocale()) {
-      $final[$dotKey]["default"] = $v;
-    }
-  }
 }
 
-echo json_encode($final);
+echo json_encode([
+    'default' => \Illuminate\Support\Facades\App::currentLocale(),
+    'translations' => $final,
+]);
