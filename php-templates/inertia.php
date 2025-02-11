6649be9f5c8bf268753ae9e14e7e455384cebc52
kimhout sam <kimhoutsam@gmail.com>
2025-02-11 12:09:25 +0700

    add php-template on inertia.php

--- php-templates/inertia.php
+++ php-templates/inertia.php
@@ -6,6 +6,11 @@ $pagePaths = collect($config['page_paths'] ?? [])->map(function($path) {
     return vsCodeToRelativePath($path);
 });
 
+$pageHints = collect(app('inertia.testing.view-finder')->getHints())->mapWithKeys(function ($value, $key) {
+    return ["{$value[0]}" => $key];
+});
+
 $config['page_paths'] = $pagePaths->toArray();
+$config['page_hints'] = $pageHints->toArray();
 
 echo json_encode($config);
