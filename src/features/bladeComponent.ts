23434764cce635a554b81cb344bcf20c92fe8914
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-03-04 16:46:15 -0500

    hover for blade components

--- src/features/bladeComponent.ts
+++ src/features/bladeComponent.ts
@@ -20,8 +20,6 @@ export const linkProvider: LinkProvider = (doc: vscode.TextDocument) => {
     lines.forEach((line, index) => {
         for (const regex of regexes) {
             const match = line.match(regex);
-            // get reflection properties for classes
-            // auto complete them + hover?
 
             if (!match || match.index === undefined) {
                 continue;
@@ -96,73 +94,47 @@ export const hoverProvider: HoverProvider = (
     doc: vscode.TextDocument,
     pos: vscode.Position,
 ): vscode.ProviderResult<vscode.Hover> => {
+    const components = getBladeComponents().items;
+    const regexes = [new RegExp(/<\/?x-([^\s>]+)/)];
+
+    if (components.prefixes.length > 0) {
+        regexes.push(
+            new RegExp(`<\\/?((${components.prefixes.join("|")})\\:[^\\s>]+)`),
+        );
+    }
+
+    for (const regex of regexes) {
+        const linkRange = doc.getWordRangeAtPosition(pos, regex);
+
+        if (!linkRange) {
+            continue;
+        }
+
+        const match = doc
+            .getText(linkRange)
+            .replace("<", "")
+            .replace("/", "")
+            .replace("x-", "");
+
+        const component = components.components[match];
+
+        if (!component) {
+            return null;
+        }
+
+        return new vscode.Hover(
+            new vscode.MarkdownString(
+                component.paths
+                    .map(
+                        (path) =>
+                            `[${path}](${
+                                vscode.Uri.file(projectPath(path)).fsPath
+                            })`,
+                    )
+                    .join("\n\n"),
+            ),
+        );
+    }
+
     return null;
-    // const links: vscode.DocumentLink[] = [];
-    // const text = doc.getText();
-    // const lines = text.split("\n");
-    // const views = getBladeComponents().items;
-
-    // lines.forEach((line, index) => {
-    //     const match = line.match(/<\/?x-([^\s>]+)/);
-
-    //     if (match && match.index !== undefined) {
-    //         const componentName = match[1];
-    //         // Standard component
-    //         const viewName = `components.${componentName}`;
-    //         // Index component
-    //         const indexName = `${viewName}.index`;
-    //         // Index component (via same name)
-    //         const sameIndexName = `${viewName}.${componentName.split(".").pop()}`;
-
-    //         const possibleNames = [
-    //             componentName,
-    //             viewName,
-    //             indexName,
-    //             sameIndexName,
-    //         ];
-
-    //         const view = views.find((v) => possibleNames.includes(v.key));
-
-    //         if (!view) {
-    //             return;
-    //         }
-
-    //         // return new vscode.Hover(
-    //         //     new vscode.MarkdownString(
-    //         //         `[${item.path}](${
-    //         //             vscode.Uri.file(projectPath(item.path)).fsPath
-    //         //         })`,
-    //         //     ),
-    //         // );
-    //         //     new vscode.DocumentLink(
-    //         //         new vscode.Range(
-    //         //             new vscode.Position(index, match.index + 1),
-    //         //             new vscode.Position(
-    //         //                 index,
-    //         //                 match.index + match[0].length,
-    //         //             ),
-    //         //         ),
-    //         //         vscode.Uri.parse(projectPath(view.path)),
-    //         //     ),
-    //         // );
-    //     }
-    // });
-
-    // return null;
-    // return Promise.resolve(links);
-    // return findHoverMatchesInDoc(doc, pos, toFind, getBladeComponents, (match) => {
-    //     const item = getBladeComponents().items.find((view) => view.key === match);
-
-    //     if (!item) {
-    //         return null;
-    //     }
-
-    //     return new vscode.Hover(
-    //         new vscode.MarkdownString(
-    //             `[${item.path}](${
-    //                 vscode.Uri.file(projectPath(item.path)).fsPath
-    //             })`,
-    //         ),
-    //     );
-    // });
 };
