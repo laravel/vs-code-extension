0b2acfe8c9e0998bbf4b217889f6f666e7d4888e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-03-10 12:56:50 -0400

    simplify inertia script

--- src/templates/inertia.ts
+++ src/templates/inertia.ts
@@ -1,12 +1,7 @@
 // This file was generated from php-templates/inertia.php, do not edit directly
 export default `
-$config = config('inertia.testing', []);
-
-$pagePaths = collect($config['page_paths'] ?? [])->map(function($path) {
-    return LaravelVsCode::relativePath($path);
-});
-
-$config['page_paths'] = $pagePaths->toArray();
-
-echo json_encode($config);
+echo json_encode([
+    ...config('inertia.testing', []),
+    'page_paths' => collect(config('inertia.testing.page_paths', []))->map(fn($path) => LaravelVsCode::relativePath($path))->toArray(),
+]);
 `;
