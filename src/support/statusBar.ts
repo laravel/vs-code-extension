3e220161e56c01c2c8c350c208972a5c8fe5fd87
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-09-02 14:09:27 -0400

    Update statusBar.ts

--- src/support/statusBar.ts
+++ src/support/statusBar.ts
@@ -5,7 +5,7 @@ export const statusBarWorking = (
     dismissAfter: number = 2000,
 ) => {
     vscode.window.setStatusBarMessage(
-        `$(loading~spin) ${message}...`,
+        `$(loading~spin) ${message}`,
         dismissAfter,
     );
 };
@@ -14,12 +14,12 @@ export const statusBarSuccess = (
     message: string,
     dismissAfter: number = 3000,
 ) => {
-    vscode.window.setStatusBarMessage(`$(check) ${message}...`, dismissAfter);
+    vscode.window.setStatusBarMessage(`$(check) ${message}`, dismissAfter);
 };
 
 export const statusBarError = (
     message: string,
     dismissAfter: number = 3000,
 ) => {
-    vscode.window.setStatusBarMessage(`$(error) ${message}...`, dismissAfter);
+    vscode.window.setStatusBarMessage(`$(error) ${message}`, dismissAfter);
 };
