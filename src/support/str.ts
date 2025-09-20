85f8b88411dde01c8735b86cd8e9cb0685084bb5
N1ebieski <kontakt@intelekt.net.pl>
2025-09-20 21:59:58 +0000

    remove numbers

--- src/support/str.ts
+++ src/support/str.ts
@@ -1,6 +1,5 @@
 export const kebab = (str: string): string =>
     str
-        .replace(/([a-z])([A-Z0-9])/g, "$1-$2")
-        .replace(/([0-9])([a-zA-Z])/g, "$1-$2")
+        .replace(/([a-z])([A-Z])/g, "$1-$2")
         .replace(/[\s_]+/g, "-")
         .toLowerCase();
