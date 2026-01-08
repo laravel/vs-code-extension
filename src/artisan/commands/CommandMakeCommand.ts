9e8339f6e17480c81ef92649b73ce81a211eba97
Titas Gailius <titas.gailius@gmail.com>
2026-01-08 16:19:41 +0200

    Add --command option

--- src/artisan/commands/CommandMakeCommand.ts
+++ src/artisan/commands/CommandMakeCommand.ts
@@ -10,5 +10,13 @@ export const CommandMakeCommand: Command = {
             description: "The name of the command",
         },
     ],
-    options: [...testOptions, forceOption],
+    options: [
+        {
+            name: "--command",
+            type: "input",
+            description: "The terminal command that will be used to invoke the class"
+        },
+        ...testOptions,
+        forceOption,
+    ],
 };
