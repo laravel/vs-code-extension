c01a744bf2c01fb1a2b73c605371d17273572937
N1ebieski <kontakt@intelekt.net.pl>
2026-01-10 10:26:45 +0000

    merge upstream main

--- src/support/str.ts
+++ src/support/str.ts
@@ -1,11 +1,11 @@
-export const lcfirst = (str: string): string =>
-    str.charAt(0).toLowerCase() + str.slice(1);
-
 export const kebab = (str: string): string =>
     str
         .replace(/([a-z])([A-Z])/g, "$1-$2")
         .replace(/[\s_]+/g, "-")
         .toLowerCase();
+
+export const lcfirst = (str: string): string =>
+    str.charAt(0).toLowerCase() + str.slice(1);
 
 export const ucfirst = (str: string): string =>
     str.charAt(0).toUpperCase() + str.slice(1);
