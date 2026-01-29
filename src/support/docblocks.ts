387c3b0444d6e46ae94abd784e0a9653b20cab47
N1ebieski <kontakt@intelekt.net.pl>
2026-01-29 12:40:25 +0000

    revert moving the json cast from array to object|array

--- src/support/docblocks.ts
+++ src/support/docblocks.ts
@@ -258,13 +258,12 @@ const getAttributeType = (attr: Eloquent.Attribute): string => {
 type TypeMapping = Record<string, (string | RegExp)[]>;
 
 const castMapping: TypeMapping = {
-    array: ["encrypted:array"],
+    array: ["json", "json:unicode", "encrypted:json", "encrypted:array"],
     int: ["timestamp"],
     mixed: ["attribute", "accessor", "encrypted"],
     object: ["encrypted:object"],
     string: ["hashed"],
     float: [/^decimal:\d+$/],
-    "object\|array": ["json", "json:unicode", "encrypted:json"],
     "\\Illuminate\\Support\\Carbon": [/^(date|datetime)(\:(.+))?$/],
     "\\Carbon\\CarbonImmutable": ["immutable_date", "immutable_datetime"],
     "\\Illuminate\\Support\\Collection": ["encrypted:collection"],
