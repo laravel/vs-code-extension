f21ed5ee6dbcb53ecf76abf97074389e6be6353c
Luke Johnson <jaewun@me.com>
2026-03-25 20:03:51 +1300

    Avoid overlapping test explorer updates (#622)
    * Avoid overlapping test explorer updates
    
    * Simplify test explorer refresh
    
    ---------
    
    Co-authored-by: Luke Johnson <luke.johnson@lightwire.co.nz>
    Co-authored-by: Titas Gailius <titas.gailius@gmail.com>

--- src/test-runner/index.ts
+++ src/test-runner/index.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 
 import { config } from "@src/support/config";
-import { createFileWatcher } from "@src/support/fileWatcher";
+import { loadAndWatch } from "@src/support/fileWatcher";
 import { updateExplorer } from "./explorer";
 import { runHandler } from "./runner";
 
@@ -28,11 +28,11 @@ export const registerTestRunner = () => {
         }
     };
 
-    createFileWatcher(["tests/**/*"], () => updateExplorer(controller), [
-        "create",
-        "delete",
-        "change",
-    ]);
-
-    updateExplorer(controller);
+    loadAndWatch(
+        () => {
+            void updateExplorer(controller);
+        },
+        ["tests/**/*"],
+        ["create", "delete", "change"],
+    );
 };
