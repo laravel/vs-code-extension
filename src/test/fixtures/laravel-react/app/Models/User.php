395a9fe16c789a8bf438cb83fe112ade59eb1585
Mariusz Wysokiński <kontakt@intelekt.net.pl>
2026-03-31 13:12:14 +0200

    Support for new Laravel 13 attributes (#626)
    * N1ebieski/vs-code-extension#97 Support for new Laravel 13 attributes
    
    ---------
    
    Co-authored-by: Titas Gailius <titas.gailius@gmail.com>

--- src/test/fixtures/laravel-react/app/Models/User.php
+++ src/test/fixtures/laravel-react/app/Models/User.php
@@ -3,11 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Attributes\Appends;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Guarded;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\Visible;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
+#[Fillable(['name'])]
+#[Guarded(['id'])]
+#[Hidden(['password'])]
+#[Visible(['name'])]
+#[Appends(['name'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
