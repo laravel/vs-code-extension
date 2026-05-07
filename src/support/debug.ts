5ad4a37115499d46f8a7cf9e6938072ca5d3420f
Titas Gailius <titas.gailius@gmail.com>
2026-05-07 12:16:57 +0300

    Update debug.ts

--- src/support/debug.ts
+++ src/support/debug.ts
@@ -1,5 +1,6 @@
 import os from "os";
-import { getCommandTemplate, runInLaravel } from "./php";
+import { sendLspRequest } from "@src/lsp/client";
+import { getCommandTemplate } from "./php";
 
 export const debugInfo: Record<string, string> = {};
 
@@ -7,19 +8,18 @@ export const collectDebugInfo = () => {
     debugInfo["os"] = os.platform();
     debugInfo["arch"] = os.arch();
 
-    runInLaravel(`
-        echo json_encode([
-            'php_version' => phpversion(),
-            'laravel_version' => app()->version(),
-        ]);
-    `).then((output) => {
+    sendLspRequest<Record<string, string>>("laravel/data", {
+        name: "debugInfo",
+    }).then((output) => {
         if (output) {
             for (const key in output as Record<string, string>) {
                 // @ts-ignore
                 debugInfo[key] = output[key];
             }
         }
-
+    }).catch((error) => {
+        console.error(error);
+    }).finally(() => {
         debugInfo["php_command"] = getCommandTemplate();
     });
 };
